c4e9ebbe577e8062eca19baa2f90f70e48967a0e
Or Shaashua <orshaashua@gmail.com>
2018-01-08 05:39:17 -0800

    check

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,30 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
 public class Controller {
 
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Text messageText;
+    @FXML
+    protected void signIn() {
+        String username = usernameTextField.getText();
+        String password = passwordField.getText();
+        if (username.equals("roi") && password.equals("123456")) {
+            messageText.setText("You are approved!");
+            messageText.setFill(Color.BLUE);
+        }
+        else {
+            messageText.setText("Invalid username or password");
+            messageText.setFill(Color.RED);
+        }
+    }
 }
