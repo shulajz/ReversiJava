467dae600f9faf5e9885ac3190bb1622ff9ce01e
Shulamit Ajzner <shulajz@gmail.com>
2018-01-15 18:09:40 +0200

    editing

--- src/reversiapp/SettingsController.java
+++ src/reversiapp/SettingsController.java
@@ -17,10 +17,8 @@ public class SettingsController {
             observableArrayList("black", "white", "blue", "green",
                     "red", "orange", "pink", "brown", "purple", "gray");
     ObservableList<String> sizes = FXCollections.
-            observableArrayList("4x4", "5x5", "6x6",
-                    "7x7", "8x8", "9x9","10x10",
-                    "11x11", "12x12", "13x13", "14x14", "15x15",
-                    "16x16", "17x17","18x18","19x19", "20x20"
+            observableArrayList("4x4", "6x6", "8x8","10x10", "12x12", "14x14",
+                    "16x16","18x18", "20x20"
                     );
     ObservableList<String> players = FXCollections.
             observableArrayList("player1", "player2");
